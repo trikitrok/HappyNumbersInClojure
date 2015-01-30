b29ee125e47662038880fcf4e896b0f9c43700cd
trikitrok <trikitrok@gmail.com>
2015-01-30 18:56:38 +0100

    Refactor: extracted helper

--- src/happy_numbers/core.clj
+++ src/happy_numbers/core.clj
@@ -1,10 +1,14 @@
 (ns happy-numbers.core)
 
 (declare 
-  parse-int)
+  parse-int,
+  square)
 
 (defn happy? [n]
-  (= (reduce + (map #(* % %)(map parse-int (str n)))) 1))
+  (= (reduce + (square (map parse-int (str n)))) 1))
 
 (defn- parse-int [ch]
   (Integer/parseInt (str ch)))
+
+(def ^:private square  
+  (partial map #(* % %)))
