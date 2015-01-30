22b551c7245a9510972d7588f53a3c4c2c7cac1c
trikitrok <trikitrok@gmail.com>
2015-01-30 19:48:26 +0100

    Optimized for speed using memoization

--- src/happy_numbers/core.clj
+++ src/happy_numbers/core.clj
@@ -1,21 +1,5 @@
 (ns happy-numbers.core)
 
-(declare 
-  parse-int
-  square
-  digits-in
-  sum-squares-of
-  one-digit?)
-
-(defn happy? [n]
-  (let [sum-squared-digits (sum-squares-of (digits-in n))]
-    (if (one-digit? sum-squared-digits)
-      (= sum-squared-digits 1)
-      (recur sum-squared-digits))))
-
-(defn happy-numbers-under [n]
-  (filter happy? (range 1 n)))
-
 (defn- parse-int [ch]
   (Integer/parseInt (str ch)))
 
@@ -30,3 +14,14 @@
 
 (defn- one-digit? [n]
   (= 1 (count (digits-in n))))
+
+(defn- happy-num? [n]
+  (let [sum-squared-digits (sum-squares-of (digits-in n))]
+    (if (one-digit? sum-squared-digits)
+      (= sum-squared-digits 1)
+      (recur sum-squared-digits))))
+
+(def happy? (memoize happy-num?))
+
+(defn happy-numbers-under [n]
+  (filter happy? (range 1 n)))
